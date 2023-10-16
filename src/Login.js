e8a63632b87ffe619bcf6dff4875ce8b944f2a45
Sahar Ali <saharnazimali@gmail.com>
2023-10-16 09:58:25 -0400

    Adds Login page

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css';
+import { loginUrl } from './spotify';
+
+function Login() {
+  return (
+    <div className="login">
+        <img 
+          src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" 
+          width={500} 
+          height={150} 
+          alt=""
+        />
+        <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+    </div>
+  )
+}
+
+export default Login
