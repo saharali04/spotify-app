48dd0f3b372ebe2bd5d7ba410e425dd12bc3a1c4
Sahar Ali <saharnazimali@gmail.com>
2023-10-16 09:59:53 -0400

    Renders login screen and authorizes user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useDataLayerValue } from "./DataLayer";
+import SpotifyWebApi from "spotify-web-api-js";
+import Login from './Login.js';
+import { getTokenFromUrl } from "./spotify";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [{ token }, dispatch] = useDataLayerValue();
+  
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+      
+      spotify.setAccessToken(_token);
+    }
+    console.log('I HAVE A TOKEN: ', token);
+  }, );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token ? (
+          <div> I am logged in </div>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
