c3fd8ff1ebe6280490ba5ac7b61e64d9b0541783
Sahar Ali <saharnazimali@gmail.com>
2023-10-20 15:38:10 -0400

    Adds PCKE auth

--- src/spotify.js
+++ src/spotify.js
@@ -10,6 +10,33 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
+export function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+export async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+  }
+  
+  
+
 export const getTokenFromUrl = () => {
     return window.location.hash
         .substring(1)
@@ -21,4 +48,4 @@ export const getTokenFromUrl = () => {
         }, {})
 }
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true&code_challenge_method=S256&code_challenge=`;
